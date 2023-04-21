36a157b3251be763df13f7cc41aed864e37c9c50
Vickyy2525 <vickyyyyy2525@gmail.com>
2023-04-21 22:54:31 +0800

    add some comment

--- AC/Basic_OOP/Basic_10.cpp
+++ AC/Basic_OOP/Basic_10.cpp
@@ -3,17 +3,17 @@ using namespace std;
   
 int main (void) {  
   int a = 0, b = 0, buf = 0;  
-  cin >> a >> b;  
-  if(a < b) {  
+  cin >> a >> b;  // input
+  if(a < b) {     // find who is bigger
     int tmp = a;  
     a = b;  
     b = tmp;  
   }  
-  while(a % b != 0 && a != 1){  
+  while(a % b != 0 && a != 1){  // find gcd
     a %= b;  
     int tmp = a;  
     a = b;  
     b = tmp;  
   }  
-  cout << b << endl;  
+  cout << b << endl;  // output!!!
 }
